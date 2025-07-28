ffdd9eac4a2a10c64449c051f9688749abc95384
TP9NS <pshccxx@gmail.com>
2025-07-28 10:13:03 +0900

    first commit

---
+++ src/main/java/com/backend/recruitAi/RecruitAiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.recruitAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitAiApplication.class, args);
+	}
+
+}
